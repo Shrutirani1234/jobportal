5aaa9384edbd669d86e855ea9afa0ef6f539b558
Shrutirani1234 <ranishruti4@gmail.com>
2024-09-23 20:18:43 +0530

    first commit

---
+++ backend1/models/job.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const jobSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    requirements:[{
+        type: String
+    }],
+    title:{
+        type: String,
+        required: true
+    },
+    salary:{
+        type: Number,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    jobType:{
+        type: String,
+        required: true
+    },
+    position:{
+        type: Number,
+        required: true
+    },
+    company:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Company'
+    },
+    created_by:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    applications:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Application'
+    }]
+    
+},{timestamps: true});
+export const Job = mongoose.model("Job", jobSchema);
---
+++ backend1/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    phonenumber:{
+        type: Number,
+        requires: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        enum: ['student','recruiter'],
+        required: true
+    },
+    profile:{
+        bio: {type: String},
+        skills: [{type: String}],
+        resume: {type: String},
+        company: {type: mongoose.Schema.Types.Objected, ref:'Company'},
+        profilePhoto:{
+            type: String,
+            default:""
+        }
+    },
+
+},{timestamps: true});
+export const User = mongoose.model('User',userSchema);
